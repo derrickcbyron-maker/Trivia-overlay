83e41477218415f7e0c01289bb175829f87fddb9
derrickcbyron-maker <derrickcbyron@gmail.com>
2026-02-24 12:33:20 -0500

    Delete questions.js

--- questions.js
+++
@@ -1,9 +0,0 @@
-// questions.js (config only)
-window.DCB_TRIVIA_CONFIG = {
-  thinkMs: 9000,
-  revealMs: 2500,
-  bufferTarget: 120,
-  fetchBatch: 50,
-  usedMax: 5000,
-  showAttributionInStatus: true
-};
